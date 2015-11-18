b5ea4c5f50263d60acbab99ec05513b9c0c3b3f3
Fred Sladkey <fsladkey@gmail.com>
2015-11-18 16:01:35 -0500

    Updated photo model

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,17 +1,20 @@
 class PhotosController < ApplicationController
 
   def create
-
+    photo = Photo.create!(photo_params)
+    render json: "GOOD JOB ME"
   end
 
   def destroy
-
+    photo = Photo.find(params[:id])
+    photo.destroy!
+    render json: "DESTROYED"
   end
 
   private
 
   def photo_params
-
+    require(:photo).permit(:photo)
   end
 
 end
---
+++ db/migrate/20151118204439_changephotocolumnname.rb
@@ -0,0 +1,5 @@
+class Changephotocolumnname < ActiveRecord::Migration
+  def change
+    
+  end
+end
